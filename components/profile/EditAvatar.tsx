e32b79d9439a10e054e20a698afa3b028e065d7e
Patrick Kjær <patrick.jessen@icloud.com>
2024-12-08 17:05:46 +0100

    Updated profile Type

--- components/profile/EditAvatar.tsx
+++ components/profile/EditAvatar.tsx
@@ -22,9 +22,18 @@ import ProfilePicture from './ProfilePicture';
 // This component can be updated to only take needed props
 // Right now it takes a whole profile, but only uses names and image url
 
+interface Profile {
+  id: string;
+  avatar_url?: string;
+  first_name?: string;
+  last_name?: string;
+  full_name?: string;
+  phone_verified?: boolean;
+  // Add any other properties that are relevant to the profile
+}
+
 interface ProfilePictureProps {
-  //Missing profile type definition
-  profile: {};
+  profile: Profile;
   size: number;
   editable: boolean;
   updateProfilePicture: () => void;
