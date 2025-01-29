b63ccc20067d654e1fe782abdcbd2310b1a8ee5b
Patrick Kjær <patrick.jessen@icloud.com>
2025-01-29 20:50:55 +0100

    Bump build

--- components/AppInfo.tsx
+++ components/AppInfo.tsx
@@ -17,7 +17,7 @@ export default function AppInfo() {
       <View style={{ gap: Spacings.xxs, alignItems: 'center' }}>
         <H4 secondary>Sanker</H4>
         <Mono secondary>{version}</Mono>
-        <Mono secondary>Build version 0.6</Mono>
+        <Mono secondary>Build version 0.7</Mono>
       </View>
       <P bold secondary>
         Hechu studio
